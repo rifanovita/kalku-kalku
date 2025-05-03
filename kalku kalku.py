8e5156ef3e6661d207d610c6a1d4ac64caf880db
rifanovita <rifanovita07@gmail.com>
2025-05-03 10:07:48 +0700

    Add files via upload

---
+++ kalku kalku.py
@@ -0,0 +1,5 @@
+print("Kalkulator Molaritas")
+mol = float(input("Masukkan jumlah mol: "))
+volume = float(input("Masukkan volume (liter): "))
+molaritas = mol / volume
+print("Molaritas =", molaritas, "mol/L")
